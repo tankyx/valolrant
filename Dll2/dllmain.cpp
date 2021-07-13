670291037c2bbd1321f0a006785b3d71f5d2f5a7
tankyx <tanguy.pedrazzoli@epitech.eu>
2021-07-13 00:47:26 -0400

    global variables

--- Dll2/dllmain.cpp
+++ Dll2/dllmain.cpp
@@ -9,6 +9,8 @@
 
 using namespace std;
 
+#define __WIDTH__ 1920.0
+#define __HEIGHT__ 1080.0
 
 int get_screen_width(void) {
     return GetSystemMetrics(SM_CXSCREEN);
@@ -63,17 +65,17 @@ BYTE* screenData = 0;
 bool run_threads = true;
 const int screen_width = get_screen_width(), screen_height = get_screen_height();
 
-int aim_x = 0;
-int aim_y = 0;
-int w = 0;
-int h = 0;
+int g_aim_x = 0;
+int g_aim_y = 0;
+int g_w = 0;
+int g_h = 0;
 
 void bot() {
     auto t_start = std::chrono::high_resolution_clock::now();
     auto t_end = std::chrono::high_resolution_clock::now();
 
     HDC hScreen = GetDC(NULL);
-    HBITMAP hBitmap = CreateCompatibleBitmap(hScreen, w, h);
+    HBITMAP hBitmap = CreateCompatibleBitmap(hScreen, g_w, g_h);
     screenData = (BYTE*)malloc(5 * screen_width * screen_height);
     HDC hDC = CreateCompatibleDC(hScreen);
     point middle_screen(screen_width >> 1, screen_height >> 1);
@@ -82,27 +84,27 @@ void bot() {
     bmi.biSize = sizeof(BITMAPINFOHEADER);
     bmi.biPlanes = 1;
     bmi.biBitCount = 32;
-    bmi.biWidth = w;
-    bmi.biHeight = -h;
+    bmi.biWidth = g_w;
+    bmi.biHeight = -g_h;
     bmi.biCompression = BI_RGB;
     bmi.biSizeImage = 0;
 
     while (run_threads) {
-        Sleep(6);
+        Sleep(3);
         HGDIOBJ old_obj = SelectObject(hDC, hBitmap);
-        BOOL bRet = BitBlt(hDC, 0, 0, w, h, hScreen, middle_screen.x - (w >> 1), middle_screen.y - (h >> 1), SRCCOPY);
+        BOOL bRet = BitBlt(hDC, 0, 0, g_w, g_h, hScreen, middle_screen.x - (g_w >> 1), middle_screen.y - (g_h >> 1), SRCCOPY);
         SelectObject(hDC, old_obj);
-        GetDIBits(hDC, hBitmap, 0, h, screenData, (BITMAPINFO*)&bmi, DIB_RGB_COLORS);
+        GetDIBits(hDC, hBitmap, 0, g_h, screenData, (BITMAPINFO*)&bmi, DIB_RGB_COLORS);
         bool stop_loop = false;
-        for (int j = 0; j < h; ++j) {
-            for (int i = 0; i < w * 4; i += 4) {
-                #define red screenData[i + (j*w*4) + 2]
-                #define green screenData[i + (j*w*4) + 1]
-                #define blue screenData[i + (j*w*4) + 0]
+        for (int j = 0; j < g_h; ++j) {
+            for (int i = 0; i < g_w * 4; i += 4) {
+                #define red screenData[i + (j*g_w*4) + 2]
+                #define green screenData[i + (j*g_w*4) + 1]
+                #define blue screenData[i + (j*g_w*4) + 0]
 
                 if (green < 190 && is_color(red, green, blue)) {
-                    aim_x = (i >> 2) - (w >> 1);
-                    aim_y = j - (h >> 1) + 3;
+                    g_aim_x = (i >> 2) - (g_w >> 1);
+                    g_aim_y = j - (g_h >> 1) + 3;
                     stop_loop = true;
                     break;
                 }
@@ -112,8 +114,8 @@ void bot() {
             }
         }
         if (!stop_loop) {
-            aim_x = 0;
-            aim_y = 0;
+            g_aim_x = 0;
+            g_aim_y = 0;
         }
     }
 }
@@ -131,14 +133,14 @@ int main(void) {
     fclose(r_f);
     FreeConsole();
     thread(bot).detach();
-    w = zone;
-    h = zone;
+    g_w = zone;
+    g_h = zone;
     auto t_start = std::chrono::high_resolution_clock::now();
     auto t_end = std::chrono::high_resolution_clock::now();
     auto left_start = std::chrono::high_resolution_clock::now();
     auto left_end = std::chrono::high_resolution_clock::now();
-    double sensitivity_x = 1.0 / sensitivity / (screen_width / 1920.0) * 1.08;
-    double sensitivity_y = 1.0 / sensitivity / (screen_height / 1080.0) * 1.08;
+    double sensitivity_x = 1.0 / sensitivity / (screen_width / __WIDTH__) * 1.08;
+    double sensitivity_y = 1.0 / sensitivity / (screen_height / __HEIGHT__) * 1.08;
     bool left_down = false;
     
     context = interception_create_context();
@@ -157,9 +159,9 @@ int main(void) {
                 if (elapsed_time_ms > 7) {
                     t_start = std::chrono::high_resolution_clock::now();
                     left_start = std::chrono::high_resolution_clock::now();
-                    if (aim_x >= -w && aim_x <= w && aim_y >= -h && aim_y <= h) {
-                        mstroke.x += double(aim_x) * sensitivity_x;
-                        mstroke.y += double(aim_y + 5) * sensitivity_y;
+                    if (g_aim_x >= -g_w && g_aim_x <= g_w && g_aim_y >= -g_h && g_aim_y <= g_h) {
+                        mstroke.x += double(g_aim_x) * sensitivity_x;
+                        mstroke.y += double(g_aim_y + 5) * sensitivity_y;
                     }
                 }
             }
@@ -168,8 +170,8 @@ int main(void) {
                 if (sensitivity <= 1.0)
                 {
                     sensitivity += 0.01;
-                    double sensitivity_x = 1.0 / sensitivity / (screen_width / 1920.0) * 1.08;
-                    double sensitivity_y = 1.0 / sensitivity / (screen_height / 1080.0) * 1.08;
+                    sensitivity_x = 1.0 / sensitivity / (screen_width / __WIDTH__) * 1.08;
+                    sensitivity_y = 1.0 / sensitivity / (screen_height / __HEIGHT__) * 1.08;
                 }
                 Sleep(40);
             }
@@ -179,8 +181,8 @@ int main(void) {
                 if (sensitivity > 0.35)
                 {
                     sensitivity -= 0.01;
-                    double sensitivity_x = 1.0 / sensitivity / (screen_width / 1920.0) * 1.08;
-                    double sensitivity_y = 1.0 / sensitivity / (screen_height / 1080.0) * 1.08;
+                    sensitivity_x = 1.0 / sensitivity / (screen_width / __WIDTH__) * 1.08;
+                    sensitivity_y = 1.0 / sensitivity / (screen_height / __HEIGHT__) * 1.08;
                 }
                 Sleep(40);
             }
